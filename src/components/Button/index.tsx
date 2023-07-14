ed157276a9d0c6f7cdd1c8e2f20889b2a04b8776
Nilton Junior <nilton.junior@digitalbusiness.com.br>
2023-07-14 17:26:51 -0300

    feat: add button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,10 @@
+import { ButtonHTMLAttributes } from "react";
+import * as S from "./styles";
+
+type ButtonProps = {
+  children?: React.ReactNode;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+const Button = ({ children, ...rest }: ButtonProps) => {
+  return <S.Button {...rest}>{children}</S.Button>;
+};
+export default Button;
