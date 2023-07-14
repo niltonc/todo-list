bba39ecfde3635e4a6283fb226a52f8341b760e1
Nilton Junior <nilton.junior@digitalbusiness.com.br>
2023-07-14 17:27:04 -0300

    feat: add input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,7 @@
+import { InputHTMLAttributes } from "react";
+import * as S from "./styles";
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
+const Input = ({ ...rest }: InputProps) => {
+  return <S.Input {...rest} />;
+};
+export default Input;
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  border: 2px solid #111;
+  border-radius: 8px;
+  box-sizing: border-box;
+  color: #111;
+  font-size: 16px;
+  height: 48px;
+  line-height: 24px;
+  max-width: 100%;
+  padding: 0 25px;
+  position: relative;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+`;
