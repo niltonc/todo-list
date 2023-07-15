a73f254c971d1a1b9bb8737c74f944a3f136a4d3
Nilton Junior <nilton.junior@digitalbusiness.com.br>
2023-07-14 23:07:06 -0300

    style: project title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
+import { AppProps } from "next/app";
+import Head from "next/head";
 
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from "styled-components";
 
-import GlobalStyles from 'styles/global';
-import theme from 'styles/theme';
+import GlobalStyles from "styles/global";
+import theme from "styles/theme";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>Project</title>
+        <title>Todo List</title>
         <link rel="shorcut icon" href="/img/bg.png" />
         <link rel="apple-touch-icon" href="/img/bg.png" />
         <link rel="manifest" href="/manifest.json" />
